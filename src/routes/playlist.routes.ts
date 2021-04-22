1c14d396db0979b5281de21c9f166e5d3c1517a7
stasrdv <stasrdv@gmail.com>
2021-04-22 11:07:58 +0300

    Refactoring and error handling

--- src/routes/playlist.routes.ts
+++ src/routes/playlist.routes.ts
@@ -1,41 +1,34 @@
 import { Router } from 'express';
-import { getYoutubeVideoInfo } from '../store/http.helper';
+import { getYoutubeVideoItem } from '../store/http.helper';
 import { YouTubeVideoController } from '../store/controllers/playlist.controller';
-import * as moment from 'moment';
-import 'moment-duration-format';
-import { YoutubeVideoItem } from '../store/models/video-item.dto';
-import { YoutubeApiResponse } from '../store/models/youtube-api-response';
+
+
 
 const playlistRouter = Router();
 const playlistController = new YouTubeVideoController();
 
 
-playlistRouter.get('/getVideos', function (req, res) {
+playlistRouter.get('/getVideos', (req, res) => {
     playlistController.getVideoItems(res);
 });
 
-playlistRouter.post('/insertVideo', function (req, res) {
+playlistRouter.post('/insertVideo', (req, res, checkBody) => {
     if (!req.body) {
         return res.status(400).json({
-            status: 'error',
-            error: 'req body cannot be empty',
+            status: 'Error',
+            error: 'Request body cannot be empty',
         });
     }
     const videoId = req.body.videoId;
-    getYoutubeVideoInfo(videoId).then((response: { data: YoutubeApiResponse }) => {
-        response.data['items'].map((item) => {
-            const duration = moment.duration(item.contentDetails.duration).format('HH:mm:ss', { trim: false });
-            const videoItem: YoutubeVideoItem = {
-                videoId: videoId,
-                title: item.snippet.title,
-                duration: duration
-
-            }
-            playlistController.saveNewVideoID(videoItem, res);
+    getYoutubeVideoItem(videoId).then(videoItem => playlistController.saveNewVideoID(videoItem, res))
+        .catch(err => {
+            return res.status(400).json({
+                status: 'Error',
+                error: err,
+            });
         });
-    })
-        .catch((error: any) => console.log(error));
 });
+
 
 
 export { playlistRouter };
--- src/store/http.helper.ts
+++ src/store/http.helper.ts
@@ -1,6 +1,29 @@
+import moment from "moment";
+import { YoutubeVideoItem } from "./models/video-item.dto";
+import { YoutubeApiResponse } from "./models/youtube-api-response";
+
 const axios = require('axios');
 
-export const getYoutubeVideoInfo = (videoId: string) => {
+const getYoutubeVideoInfo = (videoId: string) => {
     const queryUrl = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails&id=${videoId}&key=${process.env.YT_API_KEY}`
     return axios.get(queryUrl);
 }
+
+
+export const getYoutubeVideoItem = (videoId: string): Promise<YoutubeVideoItem> => {
+    return new Promise(function (resolve, reject) {
+        getYoutubeVideoInfo(videoId).then((response: { data: YoutubeApiResponse }) => {
+            response.data['items'].map((item) => {
+                const duration = moment.duration(item.contentDetails.duration).format('HH:mm:ss', { trim: false });
+                const videoItem: YoutubeVideoItem = {
+                    videoId: videoId,
+                    title: item.snippet.title,
+                    duration: duration
+                }
+                resolve(videoItem);
+            })
+        }).catch((error: any) => reject(error));
+    });
+}
+
+
